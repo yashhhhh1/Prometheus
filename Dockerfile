bffd557ee9341e1c37b629fcce6d75c4f9fd067c
yash <yash8511132161@gmail.com>
2025-06-26 10:18:28 +0530

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
 FROM python:3.10-slim
 
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+# Set the working directory
 WORKDIR /app
 
+# Install dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Copy the application code
 COPY . .
 
-ENV PORT=10000
+# Expose the port the app runs on
+EXPOSE 8000
 
-CMD ["python", "app.py"]
+# Command to run the application
+CMD ["gunicorn","-b","0.0.0.0:8000","app:app"] 
--- app.py
+++ app.py
@@ -4,14 +4,11 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 app = Flask(__name__)
 REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests")
 
-@app.route("/")
+@app.route('/')
 def home():
     REQUEST_COUNT.inc()
-    return "Hello from Flask on Render!"
+    return "Hello from Flask + Docker + Render!"
 
 @app.route("/metrics")
 def metrics():
     return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
